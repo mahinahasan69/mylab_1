6c128acc2d2697ef2f5990df8ffdefcebbd4aaba
Mahin ahasan <mahin.ahasan@knf.stud.vu.lt>
2026-09-23 23:58:59 +0300

    Added button and color changes

--- app/src/main/java/com/example/myapplicationbnew/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationbnew/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplicationbnew.ui.theme.MyApplicationBnewTheme
 
@@ -32,10 +39,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var isClicked by remember { mutableStateOf(false) }
+    val currentTextColor = if (isClicked) Color.Red else Color.Blue
+
+    Column(modifier = modifier) {
+        Text(
+            text = if (isClicked) "Button was clicked!" else "Hello $name!",
+            color = currentTextColor
+        )
+        Button(onClick = { isClicked = !isClicked }) {
+            Text("Change Color")
+        }
+    }
 }
 
 @Preview(showBackground = true)
